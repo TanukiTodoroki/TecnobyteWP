43fa7a9f205d4435e63ffd5e75742796830279e4
TanukiTodoroki <andy.chunab@gmail.com>
2021-11-09 09:44:40 -0600

    Se agrega marker
    Se agrega el marker de la ubicación física de las oficinas de tecnobyte y encabezados a la vista de servicios.

--- js/mapgoogle.js
+++ js/mapgoogle.js
@@ -1,10 +1,16 @@
 let map;
 
 function initMap() {
+  const LatLngTecnobyte = { lng: -89.6216101, lat: 21.0212196 };
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 21.0212299, lng:-89.6218066 },
     zoom: 18,
     gestureHandling: 'greedy',
     mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  new google.maps.Marker({
+    position: LatLngTecnobyte,
+    map,
+    title: "Hello World!",
   });
 }
